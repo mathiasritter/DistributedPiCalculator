6f028c2b6649b022c7373a7b7f47ac65e6cfca37
sgeyer-tgm <sgeyer@student.tgm.ac.at>
2015-01-08 17:19:26 +0100

    Fixed existing test cases

--- src/test/java/at/geyerritter/dezsys07/test/CalculatorClientTest.java
+++ src/test/java/at/geyerritter/dezsys07/test/CalculatorClientTest.java
@@ -1,6 +1,5 @@
 package at.geyerritter.dezsys07.test;
 
-import at.geyerritter.dezsys07.Calculator;
 import at.geyerritter.dezsys07.balancer.CalculatorBalancer;
 import at.geyerritter.dezsys07.client.CalculatorClient;
 import at.geyerritter.dezsys07.client.CalculatorController;
@@ -10,12 +9,11 @@ import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-
-import static org.junit.Assert.*;
 
 public class CalculatorClientTest {
 
@@ -42,12 +40,12 @@ public class CalculatorClientTest {
     public void testClient2() throws RemoteException, NotBoundException, MalformedURLException, InterruptedException {
         CalculatorBalancer cb = new CalculatorBalancer(1099);
         CalculatorServer cs = new CalculatorServer("127.0.0.1", 1099, 4567);
-        cs.registerAtRegistry();
         Thread t = new Thread(cs);
         Runtime.getRuntime().addShutdownHook(t);
         CalculatorClient c = new CalculatorClient(new CalculatorController(new ConsoleIO()));
         new Thread(c).start();
         c.getNetworkController().connect("127.0.0.1", 1099);
-        //c.getNetworkController().request(5);
+        c.getNetworkController().request(5);
+        assertEquals((String) this.appender.getLog().get(this.appender.getLog().size() - 1).getMessage(), "3.14158");
     }
 }
